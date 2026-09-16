f73da2c8fc292217cecce1aded42c266cbebee25
akashsonu0 <akashsonu7744@gmail.com>
2026-09-16 16:13:56 +0530

    HAS-A

--- 02-SpringJavaConfigApp-referenceType[HAS-A]/src/main/java/in/pwskills/akash/bean/WishMessageGenerator.java
+++ 02-SpringJavaConfigApp-referenceType[HAS-A]/src/main/java/in/pwskills/akash/bean/WishMessageGenerator.java
@@ -3,15 +3,13 @@ package in.pwskills.akash.bean;
 import java.time.LocalDateTime;
 
 //UserDefined type
-public class WishMessageGenerator {
+public class WishMessageGenerator {	
 	
 	static {
 		System.out.println("WishMessageGenerator.class file is loading...");
-	}
-	
+	}	
 	public WishMessageGenerator() {
-		System.out.println("WishMessageGenerator Object created using zero param...");
-		
+		System.out.println("WishMessageGenerator Object created using zero param...");	
 	}
 
 	//HAS-A[Inbuilt type]
